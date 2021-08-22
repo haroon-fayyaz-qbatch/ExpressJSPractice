03a2fe06d96595bb630e451536318b892aa40232
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-08-22 15:59:12 +0500

    middleware added

--- routes/messages.js
+++ routes/messages.js
@@ -52,5 +52,26 @@ router.delete('/:messageId', (req, res) => {
 });
 
 
+// a middleware sub-stack that handles GET requests to the /user/:id path
+router.get('/user/:id', function(req, res, next) {
+    console.log('HDskdsa');
+    // if the user ID is 0, skip to the next router
+    if (req.params.id === '0') next('route')
+        // otherwise pass control to the next middleware function in this stack
+    else next()
+}, function(req, res, next) {
+    console.log('ferre');
+    return res.send('GET USER');
+    // render a regular page
+    // res.render('regular')
+});
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', function(req, res, next) {
+    console.log(req.params.id)
+    res.render('special')
+});
+
+
 
 module.exports = router;
